34f29befaa35623260337e5af1a23b9fd7f7d91d
Josh Gumperz <joshg3023@gmail.com>
2021-06-26 12:03:52 -0700

    added timer section to game.html, declared variables in script.js

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+var questionEL = document.querySelector("#question")
+var choicesEl = document.querySelector(".choices")
+var timerEl = document.querySelector(".timer");
+
+var score = 0
+var userAnswer
+var questions = ["Commonly used data types do NOT include:", "The condition within an if/else statement is enclosed within ____:", "Arrays in JavaScript can be used to store:", "String values must be stored within ____ when being declared as variables:", "A very useful tool used during development and debugging for printing content out to the debugger is:"]
+var answers = {
+    question1Answers: [
+
+    ],
+    question2Answers: [
+        
+    ],
+    question3Answers: [
+
+    ],
+    question4Answers: [
+
+    ],
+    question5Answers: [
+
+    ],
+}
+var correctAnsers = []
