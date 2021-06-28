d48163f2e75ad54df537e7c59325220f1a792459
Josh Gumperz <joshg3023@gmail.com>
2021-06-28 16:15:11 -0700

    added function to sumbit when user hits enter key on endpage.html, had to do this manually cause using <form> and sumbit was causing an error. Also added content to high-scores.html, added some styling to high-scores.css, and created high-scores.js but haven't added any content to it yet.

--- assets/js/end-page.js
+++ assets/js/end-page.js
@@ -3,27 +3,34 @@ var inputEl = document.querySelector("#name-enter")
 var submitBtn = document.querySelector("#sumbit-button")
 var finalScore = document.querySelector("#final-score")
 
+// had to impliment a function to sumbit when "Enter" key is pressed manually, cause using a form in html was causing an error.
+inputContainer.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+      saveScore()
+      changePage()
+  }
+})
+
 submitBtn.addEventListener("click", function (event) {
-    event.preventDefault
-    saveScore()
-    changePage()
-  });
+  saveScore()
+  changePage()
+});
 
 function displayScore () {
-    scoreDisplay = JSON.parse(localStorage.getItem("Score"))
-    finalScore.textContent = scoreDisplay
+  scoreDisplay = JSON.parse(localStorage.getItem("Score"))
+  finalScore.textContent = scoreDisplay
 }
 
 function saveScore() {
-    var userInitials = inputEl.value.trim();
-    if (userInitials === "") {
-      return changePage()
-    }
-    storedInitials = localStorage.setItem("Initials", JSON.stringify(userInitials))
+  var userInitials = inputEl.value.trim();
+  if (userInitials === "") {
+    return changePage()
+  }
+  storedInitials = localStorage.setItem("Initials", JSON.stringify(userInitials))
 }
 
 function changePage() {
-    return window.location.assign("../html/high-scores.html")
+  return window.location.assign("../html/high-scores.html")
 }
 
-displayScore()
+displayScore() 
